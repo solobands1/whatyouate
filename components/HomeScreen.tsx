2825bba4c88f4b2edfb09b94a0336d6fc3a716be
Dillon Poulin <dillonpoulin@gmail.com>
2026-04-05 19:47:19 -0400

    polish: nudge Eat Confidently tagline slightly right

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1413,7 +1413,7 @@ export default function HomeScreen() {
               )}
             </Link>
           </div>
-          <p className="mt-1 text-[13px] text-muted/70">Eat Confidently</p>
+          <p className="mt-1 pl-0.5 text-[13px] text-muted/70">Eat Confidently</p>
           {!loadingData && mealCount === 0 && !isDemoMode && (
             <p className="mt-3 text-[11px] text-muted/60">
               Take a photo of your first meal to get started.
